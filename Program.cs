6c1d6fde994c35afea6af54d1e525495ca184f20
Masud Parvez <masud.parvez@brainstation.com>
2024-04-30 16:56:39 +0600

    Array declaration and initialization

--- Program.cs
+++ Program.cs
@@ -1,18 +1,20 @@
-﻿namespace PracticeAPp
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace PracticeAPp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Calculator calculate = new Calculator();
-            int sum = calculate.Add(15, 12);
-            int sub = calculate.Sub(15, 12);
-            Console.WriteLine($"Sum = {sum} & Sub = {sub}");
+            int[] arr = new int[3] { 1, 2, 3 };
 
-            Person person1 = new Person();
-            person1.firstName = "Masud";
-            person1.lastName = "Parvez";
-            person1.fullName();
+            foreach(int i in arr)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
