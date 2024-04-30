15a72b51c4101bc46c46915be5d9b986272bf9b1
Masud Parvez <masud.parvez@brainstation.com>
2024-04-30 16:05:28 +0600

    Create class and call class

--- Program.cs
+++ Program.cs
@@ -2,23 +2,37 @@
 {
     internal class Program
     {
+        public class Person
+        {
+            public string firstName;
+            public string lastName;
+            public void fullName()
+            {
+                Console.WriteLine($"My name is {firstName} {lastName}");
+            }
+        }
+        public class Calculator
+        {
+            public int Add(int a, int b)
+            {
+                return a + b;
+            }
+            public int Sub(int a, int b)
+            {
+                return a - b;
+            }
+        }
         static void Main(string[] args)
         {
-            // string to int
-            string s = "123";
-            int x = int.Parse(s);
-            Console.WriteLine(x);
+            Calculator calculate = new Calculator();
+            int sum = calculate.Add(15, 12);
+            int sub = calculate.Sub(15, 12);
+            Console.WriteLine($"Sum = {sum} & Sub = {sub}");
 
-            // int to string
-            int num = 123;
-            string ss = num.ToString();
-            Console.WriteLine(ss);
-
-            //float to int
-            float f = 3.5f;
-            int y = (int)f;
-            Console.WriteLine(y);
-
+            Person person1 = new Person();
+            person1.firstName = "Masud";
+            person1.lastName = "Parvez";
+            person1.fullName();
         }
     }
 }
