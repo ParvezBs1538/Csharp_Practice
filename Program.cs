434e212d31b93e01526324d7dafb447a96adfca8
Masud Parvez <masud.parvez@brainstation.com>
2024-04-30 15:15:33 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace PracticeAPp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int id = 1538;
+            float floatNumber = 3.5f;
+            decimal decimalNumber = 3.5m;
+            string name = "Masud Parvez";
+
+            Console.WriteLine(id);
+            Console.WriteLine(name);
+            Console.WriteLine(floatNumber * 2);
+            Console.WriteLine(floatNumber * 2);
+        }
+    }
+}
