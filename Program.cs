8733441ab3fe0a1ed6d89d29edd92d264dc7e237
Masud Parvez <masud.parvez@brainstation.com>
2024-05-02 15:31:23 +0600

    Extension Method

--- Program.cs
+++ Program.cs
@@ -5,35 +5,30 @@ using System.Text;
 using System.Threading.Tasks;
 namespace PracticeAPp
 {
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+    public static class Extension
+    {
+        public static string GetInfo(this Student Student) // first parameter include this keyword
+        {
+            return "ID: " + Student.Id + " Name: " + Student.Name + " Address: " + Student.Address;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Dictionary<int, string> mp = new Dictionary<int, string>();
-            mp.Add(2, "Two");
-            mp.Add(1, "One");
-            mp.Add(3, "Three");
+            Student Student = new Student();
+            Student.Id = 101;
+            Student.Name = "Parvez";
+            Student.Address = "Mirpur";
 
-            foreach (var item in mp)
-            {
-                Console.WriteLine($"{item.Key} - {item.Value}");
-            }
-            Console.WriteLine();
-
-            // assending order sort ordery by key
-            var sortAsc = mp.OrderBy(x => x.Key);
-            foreach (var item in sortAsc)
-            {
-                Console.WriteLine($"{item.Key} - {item.Value}");
-            }
-            Console.WriteLine();
-
-            // descending order sort ordery by key
-            var sortDsc = mp.OrderByDescending(x => x.Key);
-            foreach (var item in sortDsc)
-            {
-                Console.WriteLine($"{item.Key} - {item.Value}");
-            }
+            string info = Student.GetInfo();
+            Console.WriteLine(info);
         }
     }
 }
