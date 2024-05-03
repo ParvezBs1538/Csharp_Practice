e0aaca9d7e569b2e779af24b41ab9ac23b28cce5
Masud Parvez <masud.parvez@brainstation.com>
2024-05-03 19:34:03 +0600

    Object sort using Generics Constraints

--- Program.cs
+++ Program.cs
@@ -11,16 +11,40 @@ namespace PracticeAPp
         static void Main(string[] args)
         {
             //object[] arr = new object[] { 2, 1, 3, 5, 4 };
-            object[] arr = new object[] {"par", "vez", "abc", "saif"};
-            SortArray obj = new SortArray();
+            //object[] arr = new object[] {"par", "vez", "abc", "saif"};
+            Employee[] arr = new Employee[]
+            {
+                new Employee {Id = 4, Name = "par"},
+                new Employee {Id = 3, Name = "vez"},
+                new Employee {Id = 5, Name = "saif"}
+            };
+            /*SortArray obj = new SortArray();
             obj.BubbleSort(arr);
             foreach (var it in arr)
-                Console.Write(it + " ");
+                Console.Write(it + " ");*/
+            SortArray<Employee> obj = new SortArray<Employee>();
+            obj.BubbleSort(arr);
+            foreach (var it in arr)
+                Console.WriteLine((Employee)it + " ");
         }
     }
-    public class SortArray
+    public class Employee : IComparable
     {
-        public void BubbleSort(object[] arr)
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int CompareTo(object? obj)
+        {
+            return this.Id.CompareTo(((Employee)obj).Id);
+        }
+        public override string ToString()
+        {
+            return $"{Id} -> {Name}";
+        }
+    }
+    public class SortArray<T>
+    {
+        public void BubbleSort(T[] arr)
         {
             int n = arr.Length;
             for (int i = 0; i < n-1; i++)
@@ -34,9 +58,9 @@ namespace PracticeAPp
                 }
             }
         }
-        private void Swap(object[] arr, int i)
+        private void Swap(T[] arr, int i)
         {
-            object tmp = arr[i];
+            T tmp = arr[i];
             arr[i] = arr[i + 1];
             arr[i + 1] = tmp;
         }
