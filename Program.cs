9ede9d2866bf32b11fff3517c8de1cbf8c38567c
Parvezbs1538 <masud.parvez@brainstation-23.com>
2024-05-05 15:25:39 +0600

    add sub mul div using Delegate

--- Program.cs
+++ Program.cs
@@ -10,19 +10,40 @@ namespace PracticeAPp
     public delegate void Calculator(int a, int b);
     class Program
     {
-        public void Addition(int a, int b)
+        public static void Addition(int a, int b)
         {
             int ans = a + b;
             Console.WriteLine($"Addition result is : {ans}"); ;
         }
+        public static void Subtraction(int a, int b)
+        {
+            int ans = a - b;
+            Console.WriteLine($"Subtraction result is : {ans}"); ;
+        }
+        public static void Multiplication(int a, int b)
+        {
+            int ans = a * b;
+            Console.WriteLine($"Multiplication result is : {ans}"); ;
+        }
+        public static void Division(int a, int b)
+        {
+            int ans = a / b;
+            Console.WriteLine($"Division result is : {ans}"); ;
+        }
         static void Main(string[] args)
         {
-            Program program = new Program();
             //Calculator obj = new Calculator(Program.Addition);
-            Calculator obj = program.Addition;
+            Calculator obj = Program.Addition;
             obj.Invoke(20, 10);
-            program.Addition(20, 10);
+            //Program.Addition(20, 10);
+            obj = Subtraction;
+            obj(20, 10);
+
+            obj = Multiplication;
+            obj(20, 10);
+
+            obj = Division;
+            obj.Invoke(20, 10);
         }
     }
 }
-// when we dosen't use static method then you should be create program class object
