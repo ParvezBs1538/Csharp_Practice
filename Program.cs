7b38d5c037f8bb2675b6967401a74847b2ad3d98
Parvezbs1538 <masud.parvez@brainstation-23.com>
2024-05-05 15:14:00 +0600

    Delegates without use static method

--- Program.cs
+++ Program.cs
@@ -2,67 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 namespace PracticeAPp
 {
+    public delegate void Calculator(int a, int b);
     class Program
     {
+        public void Addition(int a, int b)
+        {
+            int ans = a + b;
+            Console.WriteLine($"Addition result is : {ans}"); ;
+        }
         static void Main(string[] args)
         {
-            //object[] arr = new object[] { 2, 1, 3, 5, 4 };
-            //object[] arr = new object[] {"par", "vez", "abc", "saif"};
-            Employee[] arr = new Employee[]
-            {
-                new Employee {Id = 4, Name = "par"},
-                new Employee {Id = 3, Name = "vez"},
-                new Employee {Id = 5, Name = "saif"}
-            };
-            /*SortArray obj = new SortArray();
-            obj.BubbleSort(arr);
-            foreach (var it in arr)
-                Console.Write(it + " ");*/
-            SortArray<Employee> obj = new SortArray<Employee>();
-            obj.BubbleSort(arr);
-            foreach (var it in arr)
-                Console.WriteLine((Employee)it + " ");
-        }
-    }
-    public class Employee : IComparable
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-
-        public int CompareTo(object? obj)
-        {
-            return this.Id.CompareTo(((Employee)obj).Id);
-        }
-        public override string ToString()
-        {
-            return $"{Id} -> {Name}";
-        }
-    }
-    public class SortArray<T>
-    {
-        public void BubbleSort(T[] arr)
-        {
-            int n = arr.Length;
-            for (int i = 0; i < n-1; i++)
-            {
-                for (int j = 0; j < n-i-1; j++)
-                {
-                    if (((IComparable)arr[j]).CompareTo(arr[j+1]) > 0)
-                    {
-                        Swap(arr, j);
-                    }
-                }
-            }
-        }
-        private void Swap(T[] arr, int i)
-        {
-            T tmp = arr[i];
-            arr[i] = arr[i + 1];
-            arr[i + 1] = tmp;
+            Program program = new Program();
+            //Calculator obj = new Calculator(Program.Addition);
+            Calculator obj = program.Addition;
+            obj.Invoke(20, 10);
+            program.Addition(20, 10);
         }
     }
 }
+// when we dosen't use static method then you should be create program class object
